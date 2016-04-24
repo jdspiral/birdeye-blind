63a0fd8ad77c4a70862579feec396254c0efa038
Ryan Bent <ryanbent01@gmail.com>
2016-04-23 21:14:24 -0500

    create user controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,17 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    @customer.save
+    redirect_to root_path
+  end
+
+
+private
+  def customer_params
+    params.require(:customer).permit(:name, :phone)
+  end
 end
