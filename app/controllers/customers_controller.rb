91f58b55e3a4caf8ad4416ea057ba5d566892216
Ryan Bent <ryanbent01@gmail.com>
2016-06-25 10:01:30 -0500

    change default link

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,10 +11,11 @@ class CustomersController < ApplicationController
     client = Twilio::REST::Client.new(ENV['sid'], ENV['token'])
 
     # Create and send and SMS message
+    # default at no stars https://goo.gl/N9Oqv3
     client.account.messages.create(
       from: ENV['from'],
       to: @customer.phone,
-      body: 'Thank you for visiting Sport Clips today, please give us a review - https://goo.gl/2x0c0a'
+      body: 'Thank you for visiting Sport Clips today, please give us a review - https://goo.gl/N9Oqv3'
       )
       redirect_to root_path
   end
