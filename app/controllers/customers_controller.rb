9c7d4790dbcc2b3dfba228fa8baf2c3d39d93f6b
Ryan Bent <ryanbent01@gmail.com>
2016-06-27 14:20:38 -0500

    Change review link to proper default

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,10 +12,11 @@ class CustomersController < ApplicationController
 
     # Create and send and SMS message
     # default at no stars https://goo.gl/N9Oqv3
+    # default to 5 stars https://goo.gl/cbj9qp
     client.account.messages.create(
       from: ENV['from'],
       to: @customer.phone,
-      body: 'Thank you for visiting Sport Clips today, please give us a review - https://goo.gl/N9Oqv3'
+      body: 'Thank you for visiting Sport Clips today, please give us a review - https://goo.gl/cbj9qp'
       )
       redirect_to root_path
   end
