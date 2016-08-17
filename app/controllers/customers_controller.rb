de55d305904ce4dd6a8c706806af27801ceec62e
Josh Hathcock <jdspiral@gmail.com>
2016-08-16 22:53:51 -0400

    Fix company Twilio Phone error

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -19,11 +19,16 @@ class CustomersController < ApplicationController
 
     client = Twilio::REST::Client.new('AC3d4fcc78de34a2bbc8827773e3bdc6a1', 'd4ea49fe94103e0bf7bd15dae308f1b0')
 
+    #@companyPhone = @company.where(twilio_phone: params[:twilio_phone]).first
+    company_phone = Company.find_by_sql("SELECT twilio_phone FROM companies WHERE id = #{@company}" )
+
     # Create and send and SMS message
     # default at no stars https://goo.gl/N9Oqv3
     # default to 5 stars https://goo.gl/cbj9qp
+
     client.account.messages.create(
-      from: '14692919733',
+    # from: '14692919733',
+      from: "#{company_phone[0].twilio_phone}",
       to: @customer.phone,
       body: 'Thank you for visiting Sport Clips today, please give us a review - https://goo.gl/cbj9qp'
       )
